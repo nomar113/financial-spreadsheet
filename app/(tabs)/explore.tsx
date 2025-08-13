e09e54693a79ff6195a943c63994ba0f2fa0063d
Ramon Mesquita <ramonmesquita113@gmail.com>
2025-08-12 22:55:26 -0300

    Add WebView to click in a purchase

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -3,7 +3,8 @@ import axios from 'axios';
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import React, { useCallback, useEffect, useState } from 'react';
-import { RefreshControl, SafeAreaView, SectionList, StyleSheet, View } from 'react-native';
+import { Button, RefreshControl, SafeAreaView, SectionList, StyleSheet, TouchableOpacity, View } from 'react-native';
+import WebView from 'react-native-webview';
 
 interface Purchase {
   date: string;
@@ -12,12 +13,14 @@ interface Purchase {
     date: string;
     merchantName: string;
     total: string;
+    invoiceURL: string;
   }[];
 }
 
 export default function TabTwoScreen() {
   const [purchasesGroupByDate, setPurchasesGroupByDate] = useState<Purchase[]>([]);
   const [refreshing, setRefreshing] = useState(false);
+  const [webViewURL, setWebViewURL] = useState(``)
   const fetchData = async () => {
     console.log(process.env.EXPO_PUBLIC_NOMAR_API_URL);
     try {
@@ -41,46 +44,65 @@ export default function TabTwoScreen() {
     style: 'currency',
     currency: 'BRL',
   });
+  const closeWebView = () => {
+    setWebViewURL('');
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <SectionList
-        sections={purchasesGroupByDate.map((purchase) => ({ ...purchase, data: purchase.purchases }))}
-        keyExtractor={(item, index) => item.id.toString() + index}
-        renderSectionHeader={({ section: { date } }) => (
-          <ThemedText style={styles.sectionHeader}>
-            {`${format(new Date(`${date}T12:00`), 'EEEE', { locale: ptBR })}, ${format(new Date(`${date}T12:00`), "d 'de' MMMM", { locale: ptBR })}`}
-            </ThemedText>
-        )}
-        renderItem={({ item }) => (
-          <View style={styles.card}>
-            <View style={styles.row}>
-              <View style={styles.iconColumn}>
-                <View style={styles.iconCircle}>
-                  <ThemedText style={styles.icon}>{item.merchantName.substring(0, 2)}</ThemedText>
+      {!webViewURL && 
+        <SectionList
+          sections={purchasesGroupByDate.map((purchase) => ({ ...purchase, data: purchase.purchases }))}
+          keyExtractor={(item, index) => item.id.toString() + index}
+          renderSectionHeader={({ section: { date } }) => (
+            <ThemedText style={styles.sectionHeader}>
+              {`${format(new Date(`${date}T12:00`), 'EEEE', { locale: ptBR })}, ${format(new Date(`${date}T12:00`), "d 'de' MMMM", { locale: ptBR })}`}
+              </ThemedText>
+          )}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => setWebViewURL(item.invoiceURL)}>
+              <View style={styles.card}>
+                <View style={styles.row}>
+                  <View style={styles.iconColumn}>
+                    <View style={styles.iconCircle}>
+                      <ThemedText style={styles.icon}>{item.merchantName.substring(0, 2)}</ThemedText>
+                    </View>
+                    <ThemedText style={styles.time}>{format(item.date, 'HH:mm:ss', { locale: ptBR })}</ThemedText>
+                  </View>
+                  <View style={styles.ThemedTextColumn}>
+                    <ThemedText style={styles.title}>{item.merchantName}</ThemedText>
+                    <ThemedText style={styles.subtitle}>{item.merchantName}</ThemedText>
+                  </View>
+                  <View style={styles.valueColumn}>
+                    <ThemedText style={styles.value}>{formatter.format(parseFloat(item.total))}</ThemedText>
+                  </View>
                 </View>
-                <ThemedText style={styles.time}>{format(item.date, 'HH:mm:ss', { locale: ptBR })}</ThemedText>
               </View>
-              <View style={styles.ThemedTextColumn}>
-                <ThemedText style={styles.title}>{item.merchantName}</ThemedText>
-                <ThemedText style={styles.subtitle}>{item.merchantName}</ThemedText>
-              </View>
-              <View style={styles.valueColumn}>
-                <ThemedText style={styles.value}>{formatter.format(parseFloat(item.total))}</ThemedText>
-              </View>
-            </View>
-          </View>
-        )}
-        stickySectionHeadersEnabled={false}
-        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
-        contentContainerStyle={styles.listContent}
-      />
+            </TouchableOpacity>
+          )}
+          stickySectionHeadersEnabled={false}
+          refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+          contentContainerStyle={styles.listContent}
+        />
+      }
+      {webViewURL && 
+        <View style={styles.container}>
+          <WebView
+            style={styles.container}
+            source={{uri: webViewURL}}
+          />
+          <TouchableOpacity style={styles.button}>
+            <Button title="Close WebView" onPress={closeWebView} />
+          </TouchableOpacity>
+        </View>
+      }
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0.90,
+    flex: 1,
+    justifyContent: 'center',
   },
   listContent: {
     paddingHorizontal: 16,
@@ -144,5 +166,10 @@ const styles = StyleSheet.create({
     fontSize: 15,
     fontWeight: "600",
     color: "#fff",
+  },
+  button: {
+    flex: 0.12,
+    alignSelf: 'center',
+    alignItems: 'center',
   },
 });
